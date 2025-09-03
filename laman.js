ca5b5d26e25809d8da262993252291e1c3d4fac8
admor94 <124000109+admor94@users.noreply.github.com>
2025-09-03 14:51:01 +0700

    Update laman.js

--- laman.js
+++ laman.js
@@ -1,23 +1,12 @@
-document.addEventListener(&quot;DOMContentLoaded&quot;, function() {
-    var navLinks = document.querySelectorAll(&quot;.PageList a, .LinkList a&quot;);
-
-    navLinks.forEach(function(link) {
-      var href = link.getAttribute(&quot;href&quot;);
-      // cek kalau href ada dan mengandung /p/
-      if (href &amp;&amp; href.indexOf(&quot;/p/&quot;) !== -1) {
-        link.setAttribute(&quot;target&quot;, &quot;_blank&quot;);
-        link.setAttribute(&quot;rel&quot;, &quot;noopener&quot;);
-      }
-    });
-  });
-
+<script>
 // Skrip untuk memastikan elemen #article-share benar-benar hilang di halaman statis
-document.addEventListener(&#39;DOMContentLoaded&#39;, function() {
-  var staticPageCustom = document.querySelector(&#39;.static-page-custom&#39;);
+document.addEventListener('DOMContentLoaded', function() {
+  var staticPageCustom = document.querySelector('.static-page-custom');
   if (staticPageCustom) {
-    var shareSection = document.getElementById(&#39;article-share&#39;);
+    var shareSection = document.getElementById('article-share');
     if (shareSection) {
       shareSection.remove(); // Langsung hapus elemennya dari halaman
     }
   }
 });
+</script>
