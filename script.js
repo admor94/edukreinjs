6f9814ff5523685f9668ce3d2ee63721b5f5e4ae
admor94 <124000109+admor94@users.noreply.github.com>
2025-09-04 18:14:41 +0700

    Update script.js
    Update tooltip!

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 });
+
+// Skrip untuk memanggil tooltip
+<b:if cond='!data:view.isPreview'>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+            var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+                return new bootstrap.Tooltip(tooltipTriggerEl)
+            })
+        });
+    </script>
+</b:if>
